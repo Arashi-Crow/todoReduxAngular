c3de51eebcd401f3465ba289fdd76512866db3b3
Arashi_Crow <ignaciofdce92@gmail.com>
2020-10-11 18:33:32 -0500

    filter completed

--- src/app/todos/todo-footer/todo-footer.component.ts
+++ src/app/todos/todo-footer/todo-footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducer';
+import * as actions from '../../filtro/filtro.actions';
 
 @Component({
   selector: 'app-todo-footer',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-footer.component.css']
 })
 export class TodoFooterComponent implements OnInit {
+  
+  filtroActual:actions.filtrosValidos = 'todos';
+  filtros: actions.filtrosValidos[] = ['todos', 'completados','pendientes'];
 
-  constructor() { }
+  pendientes: number = 0;
+
+  constructor(private store:Store<AppState>) { }
 
   ngOnInit(): void {
+    //this.store.select('filtro').subscribe(filtro => this.filtroActual = filtro);
+    this.store.subscribe(state => {
+      this.filtroActual = state.filtro;
+      this.pendientes = state.todos.filter( todo => !todo.completado).length;
+    })
+  }
+
+  cambiarFiltro( filtro: actions.filtrosValidos){
+     this.store.dispatch(actions.set({filter:filtro}));
   }
 
 }
--- src/app/todos/todo-list/todo-list.component.ts
+++ src/app/todos/todo-list/todo-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Todo } from '../models/todo.model';
 import { AppState } from '../../app.reducer';
+import { filtrosValidos } from '../../filtro/filtro.actions';
+import { state } from '@angular/animations';
 
 @Component({
   selector: 'app-todo-list',
@@ -9,12 +11,18 @@ import { AppState } from '../../app.reducer';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
-   arrayTodos: Todo[] = [];
+   
+  arrayTodos: Todo[] = [];
+   filtroActual:filtrosValidos;
 
   constructor(private store:Store<AppState>) { }
 
   ngOnInit(): void {
-   this.store.select('todos').subscribe( todos => this.arrayTodos = todos)
+   //this.store.select('todos').subscribe( todos => this.arrayTodos = todos);
+   this.store.subscribe(({todos,filtro}) => {
+     this.arrayTodos = todos;
+     this.filtroActual = filtro;
+   })
   }
 
 }
